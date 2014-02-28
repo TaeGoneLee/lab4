bf3abe42a36c76a548b5081b1eeee126111624e8
Sungseob Whang <sungseob_whang@brown.edu>
2014-02-27 23:05:10 -0500

    InsertionSort implemented

--- Sorting/src/Sorting/InsertionSort.java
+++ Sorting/src/Sorting/InsertionSort.java
@@ -9,7 +9,21 @@ public class InsertionSort {
 	 */
 	public static int[] insertionSort(int[] unsortedArray) {
 		//TODO: S2 implements insertion sort
-		
-		return null;
+		if ((unsortedArray==null)||(unsortedArray.length==0))
+				return unsortedArray;
+		int length = unsortedArray.length;
+		int[] result = new int[length];
+		result[0] = unsortedArray[0];
+
+		for (int i = 1; i < length; i++) {
+			int target = unsortedArray[i];
+			for (int j = 0; j < i-1; j++) if (result[j] > target) {
+				int temp = target;
+				target = result[j];
+				result[j] = temp;
+			}
+			result[i] = target;
+		}
+		return result;
 	}
 }
--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -21,13 +21,11 @@ public class Main {
 		System.out.println();
 		
 		System.out.println("Sorted Array:");
-		//TODO: sort the array and print out its contents
-		unsortedArray = BubbleSort.bubbleSort(unsortedArray);
 
-		for(int i: unsortedArray){
-			System.out.print(i+ " ");
+		int[] sorted = InsertionSort.insertionSort(unsortedArray);
+		for (int i : sorted) {
+			System.out.print(i + " ");
 		}
-		
+		System.out.println();
 	}
-
 }
