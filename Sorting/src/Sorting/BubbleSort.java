f5bec42d4920721541a2ae7038f5a9a5a646f793
TaeGoneLee <tae_gone_lee@brown.edu>
2014-02-27 23:04:15 -0500

    Bubblesort implemented

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -8,7 +8,25 @@ public class BubbleSort {
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
 		//TODO: S1 implements bubble sort
-		
-		return null;
+		if(unsortedArray == null){
+			return null;
+		}
+		int length = unsortedArray.length();
+		if(length ==1)
+			return unsortedArray;
+		for(int i=1;i<length;i++){
+			boolean swapped = false;
+			if(unsortedArray[i-1]>unsortedArray[i]){
+				int tmp = unsortedArray[i-1];
+				unsortedArray[i-1] = unsortedArray[i];
+				unsortedArray[i] = tmp;
+				swapped = true;
+			}
+			if(!swapped){
+				break;
+			}
+		}
+
+		return unsortedArray;
 	}
 }
--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -22,7 +22,11 @@ public class Main {
 		
 		System.out.println("Sorted Array:");
 		//TODO: sort the array and print out its contents
-		
+		unsortedArray = BubbleSort.bubbleSort(unsortedArray);
+
+		for(int i: unsortedArray){
+			System.out.print(i+ " ");
+		}
 		
 	}
 
